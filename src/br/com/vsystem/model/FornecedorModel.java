240221971aec106d7946704980f9aaaa736ff9b9
Iranildo Oliveira <48840372+Iranildo10@users.noreply.github.com>
2020-08-07 19:59:36 -0300

    commit

---
+++ src/br/com/vsystem/model/FornecedorModel.java
@@ -0,0 +1,81 @@
+
+package br.com.vsystem.model;
+
+
+public class FornecedorModel {
+    private int fornecedor_id;
+    private String nome_fantasia;
+    private String razao_social;
+    private String cnpj;
+    private String email;
+    private TelefoneModel telefone;
+    private EnderecoModel endereco;
+    private String apagado;
+
+    public int getFornecedor_id() {
+        return fornecedor_id;
+    }
+
+    public void setFornecedor_id(int fornecedor_id) {
+        this.fornecedor_id = fornecedor_id;
+    }
+
+    public String getNome_fantasia() {
+        return nome_fantasia;
+    }
+
+    public void setNome_fantasia(String nome_fantasia) {
+        this.nome_fantasia = nome_fantasia;
+    }
+
+    public String getRazao_social() {
+        return razao_social;
+    }
+
+    public void setRazao_social(String razao_social) {
+        this.razao_social = razao_social;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TelefoneModel getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(TelefoneModel telefone) {
+        this.telefone = telefone;
+    }
+
+    public EnderecoModel getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(EnderecoModel endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getApagado() {
+        return apagado;
+    }
+
+    public void setApagado(String apagado) {
+        this.apagado = apagado;
+    }
+    
+    
+    
+}
---
+++ src/br/com/vsystem/model/VendaModel.java
@@ -0,0 +1,79 @@
+
+package br.com.vsystem.model;
+
+
+public class VendaModel {
+    private int venda_id;
+    private ClienteModel cliente;
+    private UsuarioModel usuario;
+    private String data_venda;
+    private String observacao;
+    private double total_venda;
+    private double desconto;
+
+    public int getVenda_id() {
+        return venda_id;
+    }
+
+    public void setVenda_id(int venda_id) {
+        this.venda_id = venda_id;
+    }
+
+    public ClienteModel getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(ClienteModel cliente) {
+        this.cliente = cliente;
+    }
+
+    public UsuarioModel getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(UsuarioModel usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getData_venda() {
+        return data_venda;
+    }
+
+    public void setData_venda(String data_venda) {
+        this.data_venda = data_venda;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public double getTotal_venda() {
+        return total_venda;
+    }
+
+    public void setTotal_venda(double total_venda) {
+        this.total_venda = total_venda;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+
+    public String getStatus_venda() {
+        return status_venda;
+    }
+
+    public void setStatus_venda(String status_venda) {
+        this.status_venda = status_venda;
+    }
+    private String status_venda;
+    
+}
