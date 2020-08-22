cae86f65006c98a4af7217f2f460b58459dbdc53
Iranildo Oliveira <48840372+Iranildo10@users.noreply.github.com>
2020-08-22 16:34:42 -0300

    commit

--- src/br/com/vsystem/jdbc/ConnectionFactory.java
+++ src/br/com/vsystem/jdbc/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 public class ConnectionFactory {
     public java.sql.Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://127.0.0.1/bdvsystem","root","123456big");
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1/bdvsystem","root","moedade50");
             
         } catch (Exception erro) {
             throw new RuntimeException(erro);
